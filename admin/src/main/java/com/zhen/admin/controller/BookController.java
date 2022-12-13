e20b9329cc4bbb6309854047ac44819f94362011
zhen <215628266@qq.com>
2022-12-14 04:15:27 +0800

    测试Jenkins自动部署

--- admin/src/main/java/com/zhen/admin/controller/BookController.java
+++ admin/src/main/java/com/zhen/admin/controller/BookController.java
@@ -26,7 +26,7 @@ public class BookController {
     @GetMapping()
     public AjaxResult getAllBook() {
         List<Book> list = bookService.list();
-        return AjaxResult.success(list);
+        return AjaxResult.success("啥也没有");
     }
 
     @ApiOperation("增加图书")
